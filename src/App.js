5ac1fffcf035a4fd7eb7d8cd282bede99330c6df
Alex <AlexCarterDeveloper@gmail.com>
2019-03-14 23:00:43 +0300

    Board component, config scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import './scss/main.scss'
 import AppBar from './components/AppBar'
+import Board from './components/Board'
+
+const styleForExamples = {
+	padding: '20vh 20vw',
+	height: '60vh',
+	backgroundColor: '#eee',
+}
+
+function boardExample() {
+	var whiteCell = <div style={{backgroundColor: '#fff'}}></div>
+	var blackCell = <div style={{backgroundColor: '#000'}}></div>
+
+	var strCompPairs = {};
+	strCompPairs.w = whiteCell;
+	strCompPairs.b = blackCell;
+
+	var map = [
+		['b','w','b'],
+		['w','w','w'],
+		['w','b','w'],
+	]
+	return (
+		<div style={styleForExamples}>
+			<Board style={{height: 500, width: 500}}
+				strCompPairs={strCompPairs}
+				strField={map}
+				size={3}
+			/>
+		</div>
+	);
+}
+
 
 class App extends Component {
+
+
 	render() {
-		return (
-			<AppBar title='Tic Tac Toe' />
-		);
+		return boardExample();
 	}
 }
 
---
+++ src/components/Board.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class Board extends Component {
+	render() {
+		var { size, strCompPairs, strField} = this.props;
+
+		var cells = [];
+		strField.forEach(row => {
+			row.forEach(str => {
+				if (str in strCompPairs) {
+					cells.push(strCompPairs[str])
+				} else {
+					cells.push(<div />)
+				}
+			})
+		});
+
+		var styleColumnNum = {
+			gridTemplateColumns: `repeat(${size}, 1fr)`
+		}
+
+		return (
+			<div className="board" style={Object.assign(styleColumnNum, {color: '#ff'})}>
+				{cells}
+			</div>
+		)
+	}
+}
