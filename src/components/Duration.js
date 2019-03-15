2ebc4fb86e863e0802c13db3927e5449f4d4a35c
Alex <AlexCarterDeveloper@gmail.com>
2019-03-16 01:42:17 +0300

    Show hh mm ss

--- src/components/Duration.js
+++ src/components/Duration.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 
 export default class Duration extends Component {
+
+	convertMsToHMS(ms) {
+		return {
+			seconds: Math.floor((ms/1000)%60),
+			minutes: Math.floor((ms/(1000*60))%60),
+			hours: Math.floor((ms/(1000*60*60))%24),
+		}
+	}
+	
+	convertNumberTo2Digits(number) {
+		return  ("0" + number).slice(-2);
+	}
+
 	render() {
 		var { ms } = this.props;
+		var time = this.convertMsToHMS(ms)
+		var hh = this.convertNumberTo2Digits(time.hours);
+		var mm = this.convertNumberTo2Digits(time.minutes);
+		var ss = this.convertNumberTo2Digits(time.seconds);
 
 		return (
 			<div>
 				<p className={"duration + " + this.props.className}>
-					{ms}
+					{hh + ":" + mm + ":" + ss}
 				</p>
 			</div>
 		)
