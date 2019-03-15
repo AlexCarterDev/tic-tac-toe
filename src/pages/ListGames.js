acb58de695fe9301893f8c1dae38fc0ea1e84edb
Alex <AlexCarterDeveloper@gmail.com>
2019-03-16 00:22:08 +0300

    Stick create button to the right place

--- src/pages/ListGames.js
+++ src/pages/ListGames.js
@@ -1,57 +1,34 @@
 import React, { Component } from 'react'
 import AppBar from '../components/AppBar'
 import GamePreview from '../components/GamePreview';
+import RoundButton from '../components/RoundButton';
 
 export default class ListGames extends Component {
-
-
 	render() {
 		return (
 			<div id="list-games">
 				<AppBar>Tic Tac Toe</AppBar>
-				<div className="outer-container">
-					<div className="content-container">
-						<div className="user-name-container">
-							<input className="user-name-input" type="text"
-								placeholder="Введите свое имя" value="Chuck Norris"/>
+				<div className="content-container">
+
+					<div className="user-name-container">
+						<input className="user-name-input" type="text"
+							placeholder="Введите свое имя"/>
+					</div>
+
+					<div className="game-preview-list">
+						<div className="square">
 						</div>
-						<div className="game-preview-list">
-							<div className="square">
-								<GamePreview 
-									gameState='done'
-									owner='Criss Parrotisse'
-									opponent='Criss Parrotisse'
-									duration={12345}
-									winner='opponent'
-								/>
-							</div>
-							<div className="square">
-								<GamePreview 
-									gameState='ready'
-									owner='Criss'
-									duration={12345}
-								/>
-							</div>
-							<div className="square">
-								<GamePreview 
-									gameState='playing'
-									owner='Alex Carter'
-									opponent='John Doe'
-									duration={12345}
-								/>
-							</div>
-							<div className="square">
-								<GamePreview 
-									gameState='playing'
-									owner='Alex Carter'
-									opponent='John Doe'
-									duration={12345}
-								/>
-							</div>
+						<div className="square">
+						</div>
+						<div className="square">
+						</div>
+						<div className="square">
 						</div>
 					</div>
+
+					<RoundButton className="create-new-game">+</RoundButton>
 				</div>
-			</div>	
+			</div>
 		)
 	}
 }
